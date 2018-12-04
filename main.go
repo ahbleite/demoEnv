580f3ba042acc7f2c0350709f420995c59b21da2
Alexandre Henrique Burgos Leite <aburgos@globosat.com.br>
2018-12-04 18:44:48 -0200

    Addin health endpoint

--- main.go
+++ main.go
@@ -13,9 +13,15 @@ func home(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Ambiente: %v\n", os.Getenv("NAMESPACE"))
 }
 
+func health(w http.ResponseWriter, r *http.Request) {
+    w.WriteHeader(http.StatusOK)
+    fmt.Fprint(w, "")
+}
+
 // main function to boot up everything
 func main() {
     router := mux.NewRouter()
     router.HandleFunc("/", home).Methods("GET")
+    router.HandleFunc("/health", health).Methods("GET")
     log.Fatal(http.ListenAndServe(":9999", router))
 } 
