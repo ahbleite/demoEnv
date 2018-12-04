4c88df00c959a6b0975fc40113dbb96b78118368
Alexandre Henrique Burgos Leite <aburgos@globosat.com.br>
2018-12-04 15:47:54 -0200

    Adding API to reply the environmnet variable NAMESPACE

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+    "github.com/gorilla/mux"
+    "log"
+	"net/http"
+	"fmt"
+	"os"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+    w.WriteHeader(http.StatusOK)
+    fmt.Fprintf(w, "Ambiente: %v\n", os.Getenv("NAMESPACE"))
+}
+
+// main function to boot up everything
+func main() {
+    router := mux.NewRouter()
+    router.HandleFunc("/", home).Methods("GET")
+    log.Fatal(http.ListenAndServe(":8000", router))
+} 
