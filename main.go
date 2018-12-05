bdd66553395ebc4bad85ba07cb0dd1002c0afc0e
Alexandre Henrique Burgos Leite <aburgos@globosat.com.br>
2018-12-05 11:21:31 -0200

    Changing message

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 
 func home(w http.ResponseWriter, r *http.Request) {
     w.WriteHeader(http.StatusOK)
-    fmt.Fprintf(w, "Ambiente: %v\n", os.Getenv("ENVIRONMENT"))
+    fmt.Fprintf(w, "Ambiente: %v\nGO ninja GO!!", os.Getenv("ENVIRONMENT"))
 }
 
 func health(w http.ResponseWriter, r *http.Request) {
